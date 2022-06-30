7cd302d78709d423003ce601e9388353d612d8f1
Lucas Kaua <lucasdcunha42@gmail.com>
2022-06-30 20:25:44 -0300

    trasncriptions class

---
+++ src/main/java/Transcript.java
@@ -0,0 +1,11 @@
+public class Transcript {
+    private String audio_url;
+
+    public String getAudio_url() {
+        return audio_url;
+    }
+
+    public void setAudio_url(String audio_url) {
+        this.audio_url = audio_url;
+    }
+}
